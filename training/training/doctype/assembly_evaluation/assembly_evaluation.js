68921492a38615c4965b68d9d19be293c75b0d15
hereabdulla <hereabdulla@gmail.com>
2019-10-03 15:03:59 +0530

    updated training

--- training/training/doctype/assembly_evaluation/assembly_evaluation.js
+++ training/training/doctype/assembly_evaluation/assembly_evaluation.js
@@ -19,5 +19,63 @@ frappe.ui.form.on('Assembly Evaluation', {
                 })
             });
         }
-	}
+    },
+    refresh:function(frm){
+        if (frappe.user.has_role("Training Manager")) {
+            frm.add_custom_button(__('Send Mail to Supervisor'), function () {
+                var d = new frappe.ui.Dialog({
+                    title: __("Send Offer Mail"),
+                    'fields': [
+                        { 'fieldname': 'sp_name', 'fieldtype': 'Link', 'options': 'Employee', 'label': 'Supervisor Name'},
+                        { 'fieldname': 'col_break', 'fieldtype': 'Column Break' },
+                        { 'fieldname': 'email', 'fieldtype': 'Data', 'label': 'Email Id' },
+                        { 'fieldname': 'sec_break', 'fieldtype': 'Section Break', 'label': 'Message' },
+                        { 'fieldname': 'cc', 'fieldtype': 'Data', 'label': 'CC', 'options': 'Email' },
+                        // { 'fieldname': 'message', 'fieldtype': 'Text Editor' }
+                    ],
+                    primary_action: function () {
+                        d.hide();
+                        var args = d.get_values()
+                        args["test_score"] = frm.doc.test_score,
+                        args["productivity"] = frm.doc.productivity151,
+                        args["discipline"] = frm.doc.discipline51,
+                        args["work_quality"] = frm.doc.work_quality1,
+                        args["attendance"] = frm.doc.attendance51,
+                        args["emp"] = frm.doc.associate,
+                        args["date"] = frm.doc.date_of_skill_evaluation,
+                        args["dt"] = frm.doc.doctype,
+                        args["dn"] = frm.doc.name,
+                        args["email"] = (d.get_values()).email
+                        frappe.call({
+                            method: "training.training.doctype.assembly_evaluation.assembly_evaluation.send_mail",
+                            args: args,
+                            callback: function (r) {
+                                show_alert(__("Email sent to " + frm.doc.email))
+                            }
+                        })
+                    },
+                });
+                d.fields_dict.sp_name.input.onclick = function () {
+                    var employee = (d.get_values()).sp_name
+                    if (employee){
+                        frappe.call({
+                            method: "frappe.client.get_value",
+                            args: {
+                                doctype:"Employee",
+                                filters: {
+                                    "name":employee,
+                                },
+                                fieldname:["prefered_contact_email"]
+                            }, 
+                            callback: function(r) {
+                                console.log(r.message);
+                                d.fields_dict.email.set_value(r.message)
+                            }
+                        })
+                    }
+                }
+                d.show();
+            })
+        }
+    }
 });
--- training/training/doctype/assembly_evaluation/assembly_evaluation.py
+++ training/training/doctype/assembly_evaluation/assembly_evaluation.py
@@ -5,6 +5,8 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import add_months
+from frappe.utils.background_jobs import enqueue
 
 class AssemblyEvaluation(Document):
     def autoname(self):
@@ -21,3 +23,52 @@ def update_mis(employee,line):
     })
     doc.save(ignore_permissions=True)
     frappe.db.commit()
+
+@frappe.whitelist()
+def send_mail(**args):
+    args = frappe._dict(args)
+    next_date = add_months(args.date, 3)
+    next_d = frappe.utils.formatdate(next_date, "dd. MMMM YYYY")
+    message = """ Dear Supervisor,<br>
+    Kindly find the Test Score of the Employee (%s)"""%(args.emp)
+ 
+    table = """<table class='table table-bordered'>
+                <tr>
+                <th>Productivity (15)</th>
+                <th>Discipline (5)</th>
+                <th>Work Quality (15)</th>
+                <th>Attendance (5)</th>
+                </tr>
+                <tr>
+                <td>%s</td>
+                <td>%s</td>
+                <td>%s</td>
+                <td>%s</td>
+                </tr><br>
+                """%(args.productivity,args.discipline,args.work_quality,args.attendance)
+    # table1 = """<table class='table table-bordered'>
+    #             <tr>
+    #             <th>Test Score (30)</th>
+    #             <th>Practical Score (30)</th>
+    #             <th>Work Quality (15)</th>
+    #             <th>Attendance (5)</th>
+    #             </tr>
+    #             <tr>
+    #             <td>%s</td>
+    #             <td>%s</td>
+    #             <td>%s</td>
+    #             <td>%s</td>
+    #             </tr>"""
+    message1 = """Waiting for your Observation Scores, Result of the Evaluation is subjected to your Observation Scores.<br>Next Level Evaluation will be on %s"""%(next_d)
+    message = message + '\n' + table + '\n' + message1
+    # frappe.msgprint(message)
+    email_args = {
+        "recipients": args.email,
+        "subject": "Reg:Observation Marks",
+        "message": message,
+        "now": True,
+        }
+    if args.cc:
+        cc = { "cc":args.cc }
+        email_args.update(cc)
+    enqueue(method=frappe.sendmail, queue='short', timeout=300, async=True, **email_args)(d.get_values()).sp_name(d.get_values()).sp_name(d.get_values()).sp_name(d.get_values()).sp_name(d.get_values()).sp_name(d.get_values()).sp_name(d.get_values()).sp_name(d.get_values()).sp_name(d.get_values()).sp_name(d.get_values()).sp_name
